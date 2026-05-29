aeded4f4c3fe619e6d17ff8ffaa0beb790732277
Yimilanlan <150044594+Yimilanlan@users.noreply.github.com>
2026-05-30 00:02:48 +0800

    Add JinMangas class for comic source integration

---
+++ jinmangas.js
@@ -0,0 +1,15 @@
+class JinMangas extends ComicSource {
+
+  name = "JinMangas"
+
+  key = "jinmangas"
+
+  version = "1.0.0"
+
+  minAppVersion = "1.0.0"
+
+  url = "https://jinmangas.com"
+
+}
+
+new JinMangas()
