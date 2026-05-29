d39cc16d74a6d925be23d3fb8b3771c06756655b
Yimilanlan <150044594+Yimilanlan@users.noreply.github.com>
2026-05-30 00:06:51 +0800

    Update jinmangas.js

--- jinmangas.js
+++ jinmangas.js
@@ -10,6 +10,39 @@ class JinMangas extends ComicSource {
 
   url = "https://jinmangas.com"
 
-}
+  headers = {
+    "Referer": "https://jinmangas.com/",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+  }
 
+  async search(keyword) {
+
+    const res = await Network.get(
+      `${this.url}/?s=${encodeURIComponent(keyword)}&post_type=wp-manga`,
+      this.headers
+    )
+
+    const html = res.body
+
+    const comics = []
+
+    const reg =
+      /<a href="(https:\/\/jinmangas\.com\/manga\/.*?)".*?title="(.*?)"/gs
+
+    let match
+
+    while ((match = reg.exec(html)) !== null) {
+
+      comics.push({
+        title: match[2],
+        url: match[1]
+      })
+
+    }
+
+    return comics
+  }
+
+}
 new JinMangas()
